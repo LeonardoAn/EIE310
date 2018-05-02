60cd2746216eb3a8d43d5e1b96b13dc70ff30a21
LeonardoAn <37635246+LeonardoAn@users.noreply.github.com>
2018-05-02 01:23:18 -0300

    Rename Cap3_Eje13.m to Cap3_Eje12.m

--- Cap3_Eje12.m
+++ Cap3_Eje12.m
@@ -2,7 +2,7 @@ h = 1 : 1000; % inventa h(n)
 x = 1 : 500000; % inventa x(n)
 tic; % registra instante inicial
 [y] = conv(x, h); % convuelve
-s = toc; % tiempo de c·lculo [seg]
+s = toc; % tiempo de c√°lculo [seg]
 N = 2 * length(h) * length(x); % num. OPF
 MFps = 1E-6 * N / s; % mega flops
 fprintf('Millones ops. : %5.0f\n', 1E-6 * N)
